120cfc6d57c237c9d13c8ef0ef664938c67e2ce4
dilini-ranaweera <dilini.ranaweera.2021@gmail.com>
2025-03-18 19:53:04 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ import './index.css';
 
 const App = () => {
   const philadelphiaForecastData = [
-    { day: "Sunday, February 9th", icon: "assets/sun.png", high: "60°F @ 2:43 PM", low: "47°F @ 6:00 AM" },
-    { day: "Monday, February 10th", icon: "assets/rain.png", high: "75°F @ 4:24 PM", low: "50°F @ 7:10 AM" },
-    { day: "Tuesday, February 11th", icon: "assets/sun.png", high: "71°F @ 4:21 PM", low: "55°F @ 3:00 AM" },
-    { day: "Wednesday, February 12th", icon: "assets/sun.png", high: "78°F @ 11:00 AM", low: "61°F @ 1:00 AM" },
-    { day: "Thursday, February 13th", icon: "assets/sun.png", high: "75°F @ 3:33 PM", low: "55°F @ 8:00 AM" },
-    { day: "Friday, February 14th", icon: "assets/cloudy.png", high: "80°F @ 6:40 PM", low: "52°F @ 6:00 AM" },
-    { day: "Saturday, February 15th", icon: "assets/rain.png", high: "90°F @ 1:20 PM", low: "65°F @ 5:55 AM" },
+    { day: "Sunday, February 9th", icon: "src/assets/sun.png", high: "60°F @ 2:43 PM", low: "47°F @ 6:00 AM" },
+    { day: "Monday, February 10th", icon: "src/assets/rain.png", high: "75°F @ 4:24 PM", low: "50°F @ 7:10 AM" },
+    { day: "Tuesday, February 11th", icon: "src/assets/sun.png", high: "71°F @ 4:21 PM", low: "55°F @ 3:00 AM" },
+    { day: "Wednesday, February 12th", icon: "src/assets/sun.png", high: "78°F @ 11:00 AM", low: "61°F @ 1:00 AM" },
+    { day: "Thursday, February 13th", icon: "src/assets/sun.png", high: "75°F @ 3:33 PM", low: "55°F @ 8:00 AM" },
+    { day: "Friday, February 14th", icon: "src/assets/cloudy.png", high: "80°F @ 6:40 PM", low: "52°F @ 6:00 AM" },
+    { day: "Saturday, February 15th", icon: "src/assets/rain.png", high: "90°F @ 1:20 PM", low: "65°F @ 5:55 AM" },
   ];
 
   return (
